9d64e4c6e30f55ff93b6c747829e6f952d30f5fe
coderdojotacoma <tacoma.us@coderdojo.com>
2017-04-19 12:49:17 -0700

    Create tour.py

---
+++ tour.py
@@ -0,0 +1,18 @@
+# Connect to Minecraft
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import time
+# Set x, y, and z variables to represent coordinates
+x = # Fill in
+y = # Fill in
+z = # Fill in
+# Change the player's position
+mc.player.setTilePos(x, y, z)
+# Wait 10 seconds
+time.sleep(10)
+# Set x, y, and z variables to represent coordinates
+x = # Fill in
+y = # Fill in
+z = # Fill in
+# Change the player's position
+mc.player.setTilePos(x, y, z)
